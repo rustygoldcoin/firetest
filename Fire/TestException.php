9f56176062a069397ff2739b2baa02fc60c27027
Joshua L Johnson <josh@ua1.us>
2018-04-14 01:03:22 -0400

    added copyright notice

--- Fire/TestException.php
+++ Fire/TestException.php
@@ -1,4 +1,19 @@
 <?php
+
+/**
+ *    __  _____   ___   __          __
+ *   / / / /   | <  /  / /   ____ _/ /_  _____
+ *  / / / / /| | / /  / /   / __ `/ __ `/ ___/
+ * / /_/ / ___ |/ /  / /___/ /_/ / /_/ (__  )
+ * `____/_/  |_/_/  /_____/`__,_/_.___/____/
+ *
+ * @package FireStudio
+ * @subpackage FireTest
+ * @author UA1 Labs Developers https://ua1.us
+ * @copyright Copyright (c) UA1 Labs
+ */
+
+
 namespace Fire;
 
 use Exception;
--- scripts/runner.php
+++ scripts/runner.php
@@ -1,4 +1,18 @@
 <?php
+
+/**
+ *    __  _____   ___   __          __
+ *   / / / /   | <  /  / /   ____ _/ /_  _____
+ *  / / / / /| | / /  / /   / __ `/ __ `/ ___/
+ * / /_/ / ___ |/ /  / /___/ /_/ / /_/ (__  )
+ * `____/_/  |_/_/  /_____/`__,_/_.___/____/
+ *
+ * @package FireStudio
+ * @subpackage FireTest
+ * @author UA1 Labs Developers https://ua1.us
+ * @copyright Copyright (c) UA1 Labs
+ */
+
 require_once __DIR__ . '/../../../autoload.php';
 
 $options = getopt('', ['dir:', 'ext:']);
